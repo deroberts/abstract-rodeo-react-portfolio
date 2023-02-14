aaa24525320303cf92dbd0d6748517ce01af656f
deroberts <s501656@gmail.com>
2023-02-14 13:56:02 -0600

    added react files to update portfolio.

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+import { FaUserAstronaut } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="headerH1"></h1>
+      <p style={{ textAlign: 'center' }}>(Junior Web Developer)</p>
+    </div>
+  );
+};
+
+export default Header;
