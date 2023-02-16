81fe6e1cb566468a2a5733fa7313d82f01615b83
deroberts <s501656@gmail.com>
2023-02-16 16:31:36 -0600

    okay! the router, routes, route files are now configured and I can finally play the game

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import reactDOM from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const root = reactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><App /></React.StrictMode>);
+
